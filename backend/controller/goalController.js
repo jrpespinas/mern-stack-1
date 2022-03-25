bd3d252e86448cdbd8284d3aaf11a071222086ba
Bogs Espinas <jrpespinas@gmail.com>
2022-03-25 23:53:05 +0800

    feat: implement error handler

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -21,6 +21,11 @@ const getGoals = (req, res) => {
  *
  */
 const setGoals = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res.status(200).json({ message: "Set goals from controller" });
 };
 
