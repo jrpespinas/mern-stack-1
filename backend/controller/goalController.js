e22a7c6029a458e5f8feb0922076aa169c411c6c
Bogs Espinas <jrpespinas@gmail.com>
2022-03-25 22:48:57 +0800

    feat: implement controller endpoints

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,5 +1,7 @@
 /**
- * Display all recorded goals.
+ * Displays all recorded goals.
+ *
+ * @access private
  *
  * @param {string} req request
  * @param {string} res response
@@ -9,6 +11,52 @@ const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals from controller" });
 };
 
+/**
+ * Creates a goal.
+ *
+ * @access private
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const setGoals = (req, res) => {
+  res.status(200).json({ message: "Set goals from controller" });
+};
+
+/**
+ * Updates a goal given an id.
+ *
+ * @access private
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const putGoal = (req, res) => {
+  res.status(200).json({
+    message: `Update goal ${req.params.id} from controller`,
+  });
+};
+
+/**
+ * Deletes a goal given an id.
+ *
+ * @access private
+ *
+ * @param {string} req request
+ * @param {string} res response
+ *
+ */
+const deleteGoal = (req, res) => {
+  res.status(200).json({
+    message: `Update goal ${req.params.id} from controller`,
+  });
+};
+
 module.exports = {
   getGoals,
+  setGoals,
+  putGoal,
+  deleteGoal,
 };
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,25 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals } = require("../controller/goalController");
+const {
+  getGoals,
+  setGoals,
+  putGoal,
+  deleteGoal,
+} = require("../controller/goalController");
 
 router.get("/", getGoals);
-
-router.post("/", (req, res) => {
-  res.status(200).json({
-    message: "Set goals",
-  });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    message: `Update goal ${req.params.id}`,
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    message: `Delete goal ${req.params.id}`,
-  });
-});
+router.post("/", setGoals);
+router.put("/:id", putGoal);
+router.delete("/:id", deleteGoal);
 
 module.exports = router;
