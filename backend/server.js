482dda86d6cafd22c4f59fbde3908d9176b3c72c
Bogs Espinas <jrpespinas@gmail.com>
2022-03-25 16:38:34 +0800

    feat: implement initial route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/api/goals", (request, response) => {
+  response.status(200).json({
+    message: "Get goals",
+  });
+});
+app.listen(port, () => console.log(`Server started at ${port}`));
